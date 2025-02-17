63c6f1652378a2c5860efe88e8879566bd3eaf82
jaibwii <jbrwoj32@gmail.com>
2025-02-17 13:10:07 +0800

    forms created with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form';
+  loginForm: FormGroup;
+  formSubmitted: boolean = false;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      name: new FormControl('', [Validators.required]),
+      contactNumber: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(11)]),
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.formSubmitted = true;
+      console.log('Form Submitted.', this.loginForm.value);
+    } else {
+      console.log('Form is invalid.');
+    }
+  }
 }
